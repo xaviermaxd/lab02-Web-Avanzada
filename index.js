55d8e9e3e13bfe0ac268d44aae24b4263bf3f726
Xavier Mamani <xavier.mamani@tecsup.edu.pe>
2023-09-04 11:43:31 -0500

    tarea - lab02

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req, res) => {
+    res.sendFile('./scripts/index.html', {
+        root: __dirname
+    })
+})
+
+
+app.get('/contacto', (req, res) => { 
+    res.json({
+        name: 'Empresa abc',
+        direction: 'ubicado en la avenida.....',
+        phone: '999-999-999'
+    });
+});
+
+app.get('/formulario', (req, res) => { 
+    res.sendFile('./scripts/formulario.html', {
+        root: __dirname
+    })
+});
+
+app.post('/formulario', (req, res) => { 
+    console.log(req.body);
+    res.send('Reclamo guardado!!!');
+});
+
+app.use((req,res) => {
+    res.status(404).send('No se encontro tu pagina....!!!')
+})
+
+app.listen(3000)
+console.log(`Server on port ${3000}`)
